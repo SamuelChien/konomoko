fd5a7425a162e60a556dfbded6945328415b4f32
Samuel Chien <samuelchien821@gmail.com>
2017-11-13 01:01:40 -0800

    Clean up old unused route file

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const mongo = require('mongodb');
 const mongoose = require('mongoose');
+//Initialize connection once (localhost "mongodb://localhost:27017/konomoko")
+mongoose.connect('mongodb://konomoko:BRuFfI4XaJ6Y5vIwaMbLiMe2CSAoZCHTa0ktQWspQ9uoxkFInOcHsL3ak9sIeirMHW6hcWJ80OcVBMgCaN0yGQ==@konomoko.documents.azure.com:10255/konomoko?ssl=true', { useMongoClient: true,});
 
 // Configuring Passport
-const expressValidator = require('express-validator');
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const User = require('./models/user');
-
-
-//Initialize connection once (localhost "mongodb://localhost:27017/konomoko")
-mongoose.connect('mongodb://konomoko:BRuFfI4XaJ6Y5vIwaMbLiMe2CSAoZCHTa0ktQWspQ9uoxkFInOcHsL3ak9sIeirMHW6hcWJ80OcVBMgCaN0yGQ==@konomoko.documents.azure.com:10255/konomoko?ssl=true', { useMongoClient: true,});
-const db = mongoose.connection;
-
 const index = require('./routes/index')(passport);
-const users = require('./routes/users');
 const reports = require('./routes/reports');
-const dashboard = require('./routes/dashboard');
 const login = require('./routes/login');
 const stripe = require('./routes/stripe');
 const upload = require('./routes/upload');
@@ -35,66 +24,32 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-// Configuring Passport
 // Express Session
-app.use(session({
-    secret: 'secret',
-    saveUninitialized: true,
-    resave: true
+    app.use(session({
+            secret: 'secret',
+        saveUninitialized: true,
+        resave: true
 }));
+
 // Passport init
 app.use(passport.initialize());
 app.use(passport.session());
-// Express Validator
-app.use(expressValidator({
-    errorFormatter: function(param, msg, value) {
-        var namespace = param.split('.')
-            , root    = namespace.shift()
-            , formParam = root;
-
-        while(namespace.length) {
-            formParam += '[' + namespace.shift() + ']';
-        }
-        return {
-            param : formParam,
-            msg   : msg,
-            value : value
-        };
-    }
-}));
-// Connect Flash
-app.use(flash());
-
-// =============  Begin: Initialize Passport ================
-// Initialize Passport
 var initPassport = require('./passport/init');
 initPassport(passport);
 
-// =============  End: Initialize Passport ================
-
-// Global Vars
-app.use(function (req, res, next) {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    res.locals.user = req.user || null;
-    next();
-});
+//initialize flash
+app.use(flash());
 
 
 app.use('/', index);
-app.use('/users', users);
 app.use('/reports', reports);
-app.use('/dashboard', dashboard);
 app.use('/login', login);
 app.use('/stripe', stripe);
 app.use('/upload', upload);
@@ -118,15 +73,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-// route middleware to make sure a user is logged in
-function isLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on 
-    if (req.isAuthenticated())
-        return next();
-
-    // if they aren't redirect them to the home page
-    res.redirect('/');
-}
 
 module.exports = app;
